33551f55bf86da73985494451c7675a76c1abeaf
Daniel Olea <dani@scalaros.com>
2024-08-30 08:19:44 +0200

    Changes

--- main.py
+++ main.py
@@ -18,36 +18,36 @@ contacts_collection = db.get_collection('contacts')
 # Function to process the given company ID
 def process_company_contacts(company_id, user, password):
     try:
-        try:
-            account_object_id = ObjectId(company_id)
-        except Exception as e:
-            print("Invalid company ID format.")
-            return
+      try:
+          account_object_id = ObjectId(company_id)
+      except Exception as e:
+          print("Invalid company ID format.")
+          return
 
-        # Query MongoDB for contacts with the given account_id
-        contacts = contacts_collection.find({"accountId": account_object_id}).limit(300)
-        processed_contacts = []
+      # Query MongoDB for contacts with the given account_id
+      contacts = contacts_collection.find({"accountId": account_object_id}).limit(300)
+      processed_contacts = []
 
-        for index, contact in enumerate(contacts):
-            print(f"Starting processing of contact #{index}")
-            contact_data = {
-                'contact_name': contact.get('rawObject', {}).get('full_name'),
-                'contact_email': contact.get('rawObject', {}).get('email'),
-                'company_name': contact.get('rawObject', {}).get('company_name'),
-                'contact_linkedin_url': contact.get('rawObject', {}).get('linkedin_url', None),
-            }
+      for index, contact in enumerate(contacts):
+          print(f"Starting processing of contact #{index}")
+          contact_data = {
+              'contact_name': contact.get('rawObject', {}).get('full_name'),
+              'contact_email': contact.get('rawObject', {}).get('email'),
+              'company_name': contact.get('rawObject', {}).get('company_name'),
+              'contact_linkedin_url': contact.get('rawObject', {}).get('linkedin_url', None),
+          }
 
-            print(f"Processing contact: {contact_data}")
+          print(f"Processing contact: {contact_data}")
 
-            # Process each contact
-            processed_contact = process_crm_contact(contact_data, user, password)
-            processed_contacts.append(processed_contact)
+          # Process each contact
+          processed_contact = process_crm_contact(contact_data, user, password)
+          processed_contacts.append(processed_contact)
 
-        print("Processed Contacts:")
-        for contact in processed_contacts:
-            print(contact)
+      print("Processed Contacts:")
+      for contact in processed_contacts:
+          print(contact)
     except Exception as e:
-        print(f"An error occurred: {e}")
+      print(f"An error occurred: {e}")
 
 
 def main():
